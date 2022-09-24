51e52f1e067a7e93ad694321bf7234d949a587bf
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-09-24 17:20:10 +0500

    Update AllDiscountedProducts.js

--- NewProject/screens/AllDiscountedProducts.js
+++ NewProject/screens/AllDiscountedProducts.js
@@ -1,17 +1,28 @@
-import { useNavigation } from '@react-navigation/native';
-import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList, ActivityIndicator } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import SkeletonJs from '../components/Skeleton'
+import SkeletonJs from '../components/Skeleton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NativeBaseProvider } from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 import SearchBar from '../components/SearchBar';
 
-
 const AllDiscountedProducts = () => {
-  const navigate = useNavigation()
-  const [isAdded,setIsAdded]=useState(false)
+  const navigate = useNavigation();
+  const [isAdded, setIsAdded] = useState(false);
+
+  const [asynData, setAsynData] = useState([]);
+  // console.log("🚀 ~ file: AllDiscountedProducts.js ~ line 24 ~ AllDiscountedProducts ~ asynData", asynData[0].product_id)
+  const [FavProducts, setFavProducts] = useState([]);
 
   const [products, setProducts] = useState([]);
   // console.log("🚀 ~ file: AllDiscountedProducts.js ~ line 16 ~ AllDiscountedProducts ~ products", products)
@@ -19,45 +30,72 @@ const AllDiscountedProducts = () => {
   const [isLoading, setIsloading] = useState(true);
   const [IsRefreshing, setIsRefreshing] = useState(false);
 
+  const getDisdata = async () => {
 
-
-  const getdata = async () => {
-    setIsloading(true)
+    setIsloading(true);
     await fetch(`http://192.168.1.17:5000/sql/allDiscountProducts/${limit}`)
-      .then((response) => response.json())
-      .then((json) => { setProducts(json) })
-      .catch((error) => console.error(error))
-
-  }
+      .then(response => response.json())
+      .then(json => {
+        setProducts(json);
+      })
+      .catch(error => console.error(error));
+  };
 
   useEffect(() => {
-    getdata()
+    getDisdata();
   }, [limit]);
+
+
 
   const onEndReached = () => {
     setlimit(limit + 4);
-    setIsloading(false)
-  }
+    setIsloading(false);
+  };
+
 
   const onRefresh = () => {
     setIsRefreshing(true);
     setProducts([]);
     setlimit(6);
-    setIsRefreshing(false)
-  }
+    setIsRefreshing(false);
+  };
+
 
   const flatlistEnd = () => {
-    return (
-      isLoading ?
-        <NativeBaseProvider>
-          <View>
-            <SkeletonJs />
-          </View>
-        </NativeBaseProvider> : null
-    );
-  }
+    return isLoading ? (
+      <NativeBaseProvider>
+        <View>
+          <SkeletonJs />
+        </View>
+      </NativeBaseProvider>
+    ) : null;
+  };
 
-  const addToCart = async (productData) => {
+
+
+
+  
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@cartItems');
+      const result = JSON.parse(jsonValue);
+      setAsynData(result);
+
+      // return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (error) {
+      alert('Something went wrong[[[[[[[[[[[[[[[');
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+
+
+  const addToCart = async productData => {
     try {
       let asyncData = await AsyncStorage.getItem('@cartItems');
       asyncData = JSON.parse(asyncData);
@@ -65,75 +103,142 @@ const AllDiscountedProducts = () => {
         let cartItem = asyncData;
         cartItem.push(productData);
         await AsyncStorage.setItem('@cartItems', JSON.stringify(cartItem));
-      }
-      else {
+      } else {
         let cartItem = [];
         cartItem.push(productData);
         await AsyncStorage.setItem('@cartItems', JSON.stringify(cartItem));
       }
     } catch (error) {
-      alert('Something went wrong');
+      alert('Something went wrongXXXXXXXXXXXX');
     }
-  }
+  };
 
-  const addToFav = async (productDetail) => {
+  const removeSpecificProduct = async productData => {
+    try {
+      let asyncData = await AsyncStorage.getItem('@cartItems');
+      asyncData = JSON.parse(asyncData);
+      if (asyncData) {
+        let cartItem = asyncData;
+        const removedData = cartItem.filter(
+          object => object.product_id != productData.product_id,
+        );
+        const removeData = FavProducts.filter(
+          object => object.product_id != productData.product_id,
+        );
+        setFavProducts(removeData);
+        await AsyncStorage.removeItem('@cartItems');
+        await AsyncStorage.setItem('@cartItems', JSON.stringify(removedData));
+      }
+    } catch (error) {
+      alert('Something went wrong=========>');
+    }
+  };
+
+  const addToFav = async productDetail => {
     try {
       addToCart(productDetail);
-     
-      alert('added to fav');
+      // setIsAdded(!isAdded);
+      alert('added to fav');//======================
     } catch (error) {
       alert(error);
       // alert('add to fav failed');
     }
-  }
+  };
 
-  const renderItem = (element) => {
+  const removeFav = async productDetail => {
+    try {
+      removeSpecificProduct(productDetail);
+      // setIsAdded(!isAdded);
+      alert('remove from Fav');//======================
+    } catch (error) {
+      alert(error);
+      // alert('add to fav failed');
+    }
+  };
+
+  const renderItem = element => {
+
+    
 
 
     const productDetail = {
       product_id: element.item.product_id,
       name: element.item.name,
       price: element.item.price,
-      image: element.item.imgs
-    }
+      image: element.item.imgs,
+    };
+
+    const isFavourate = id =>
+      Boolean(asynData.find(item => item.product_id === id));
 
     return (
       <View style={Style.all_item_main2}>
         <View style={Style.all_item_main3}>
-          <TouchableOpacity style={Style.all_item_main4} onPress={() => navigate.navigate('Product_detail', element.item)}>
+          <TouchableOpacity
+            style={Style.all_item_main4}
+            onPress={() => navigate.navigate('Product_detail', element.item)}>
             <View>
-                <Text
-                  style={Style.discount}>
-                  {element.item.discount_percent}%
-                </Text>
-              </View>
-            <Image style={Style.all_item_main4_img}
+              <Text style={Style.discount}>
+                {element.item.discount_percent}%
+              </Text>
+            </View>
+
+            {Boolean(asynData.find(item => item.product_id === element.item.product_id)) ? (
+              <MaterialCommunityIcons
+                name="cards-heart"
+                onPress={() => {removeFav(productDetail);}}
+                style={Style.middle2_2_icon}
+              />
+            ) : (
+              <MaterialCommunityIcons
+                name="cards-heart-outline"
+                onPress={() => {addToFav(productDetail);}}
+                style={Style.middle2_2_icon}
+              />
+            )}
+            {/* {isFavourate(element.item.product_id) ? (
+              <MaterialCommunityIcons
+                name="cards-heart"
+                onPress={() => {removeFav(productDetail);}}
+                style={Style.middle2_2_icon}
+              />
+            ) : (
+              <MaterialCommunityIcons
+                name="cards-heart-outline"
+                onPress={() => {addToFav(productDetail);}}
+                style={Style.middle2_2_icon}
+              />
+            )} */}
+
+            <Image
+              style={Style.all_item_main4_img}
               resizeMode="cover"
-              source={{ uri: element.item.imgs }}
+              source={{uri: element.item.imgs}}
             />
             <View>
               <Text style={Style.cardTitle}>
-                {element.item.name.split(/\s+/).slice(0, 4).join(" ") + "..."}
+                {element.item.name.split(/\s+/).slice(0, 4).join(' ') + '...'}
               </Text>
               <View style={Style.cardBotm}>
-                <Text
-                  style={Style.cardPrice}>
-                  RS. {element.item.price}
-                </Text>
-                
-                {/* add product to fav */}
-            <MaterialCommunityIcons name="cards-heart-outline" onPress={() => addToFav(productDetail)} style={Style.middle2_2_icon} />
+                <Text style={Style.cardPrice}>RS. {element.item.price}</Text>
+
+                {/* add product to fav          */}
+                {/* {
+                  isAdded?
+                  <MaterialCommunityIcons name="cards-heart"   onPress={() => removeFav(productDetail)} style={Style.middle2_2_icon} />
+                  :
+                  <MaterialCommunityIcons name="cards-heart-outline" onPress={() => addToFav(productDetail)} style={Style.middle2_2_icon} />
+                } */}
               </View>
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity 
-          // onPress={() => addToCart(productDetail)}
-           style={Style.fav_icon_box}>
-          <Text style={Style.rating}>
-                  4.5{' '}
-                  <Icon style={Style.ratingIcon} name="md-star-half-sharp" />
-                </Text>
+          <TouchableOpacity
+            // onPress={() => addToCart(productDetail)}
+            style={Style.fav_icon_box}>
+            <Text style={Style.rating}>
+              4.5 <Icon style={Style.ratingIcon} name="md-star-half-sharp" />
+            </Text>
             {/* <MaterialCommunityIcons name="cards-heart-outline" style={Style.middle2_2_icon} /> */}
           </TouchableOpacity>
 
@@ -152,11 +257,10 @@ const AllDiscountedProducts = () => {
           {/*<TouchableOpacity onPress={() => storeMergeData(product)} style={{ flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "white", borderWidth: 1, elevation: 2, height: 35, borderRadius: 22, marginBottom: 4 }}>
           <Feather name="flower" style={Style.middle2_2_icon} />
         </TouchableOpacity> */}
-
         </View>
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <View style={Style.all_item_main}>
@@ -167,42 +271,49 @@ const AllDiscountedProducts = () => {
             <Text style={Style.mainHead}>All Discounts</Text>
           </View>
         }
-        data={products} renderItem={renderItem} keyExtractor={item => item.product_id} numColumns={2}
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.product_id}
+        numColumns={2}
         ListFooterComponent={flatlistEnd}
-        onEndReached={onEndReached} onEndReachedThreshold={0.5} refreshing={IsRefreshing} onRefresh={onRefresh} />
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.5}
+        refreshing={IsRefreshing}
+        onRefresh={onRefresh}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default AllDiscountedProducts
+export default AllDiscountedProducts;
 
 const Style = StyleSheet.create({
-  fav_icon_box:{
+  fav_icon_box: {
     // backgroundColor:'pink',
-    alignItems:'center',
-    alignSelf: "flex-end",
+    alignItems: 'center',
+    alignSelf: 'flex-end',
   },
   discount: {
-    padding:2,
-    marginLeft:-5,
-    marginTop:-5,
-    textAlign:'center',
-    backgroundColor:'red',
-    color:'#fff',
-    position:'relative',
-    left:5,
-    top:5,
+    padding: 2,
+    marginLeft: -5,
+    marginTop: -5,
+    textAlign: 'center',
+    backgroundColor: 'red',
+    color: '#fff',
+    position: 'relative',
+    left: 5,
+    top: 5,
+    zIndex: 5,
     borderRadius: 6,
     fontWeight: 'bold',
-    fontSize:15,
-    width:50,
-
-  }, middle2: {
+    fontSize: 15,
+    width: 50,
+  },
+  middle2: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingLeft: 10,
-
   },
   middle2_1: {
     flexDirection: 'row',
@@ -231,21 +342,26 @@ const Style = StyleSheet.create({
     marginRight: 4,
   },
   middle2_2_icon: {
-    margin:2,
-    fontSize: 25,
+    // backgroundColor:'pink',
+    position: 'relative',
+    left: '81%',
+    top: '-10%',
+    zIndex: 6,
+    margin: 2,
+    fontSize: 28,
     marginRight: 10,
-    color: "#5A56E9",
+    color: '#5A56E9',
     fontWeight: 'bold',
   },
   all_item_main: {
     flex: 1,
-    width: "100%",
-    backgroundColor: "#e8e7e6",
+    width: '100%',
+    backgroundColor: '#e8e7e6',
   },
   all_item_main2: {
     width: '50%',
     padding: 4,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   all_item_main3: {
     padding: 5,
@@ -258,19 +374,19 @@ const Style = StyleSheet.create({
     borderBottomWidth: 0,
     // justifyContent: 'center',
     // alignItems: 'center',
-    borderBottomColor: "#ACACAC",
+    borderBottomColor: '#ACACAC',
     paddingBottom: 8,
     //backgroundColor:'green'
   },
   all_item_main4_img: {
     width: '90%',
     height: 120,
-    marginLeft:10,
+    marginLeft: 10,
   },
   cardTitle: {
     margin: 1,
     color: 'black',
-    fontSize:15
+    fontSize: 15,
   },
   cardPrice: {
     fontSize: 17,
@@ -288,15 +404,15 @@ const Style = StyleSheet.create({
   cardBotm: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginVertical: "2%",
-    paddingLeft: "1%",
-    alignItems: "center"
+    marginVertical: '2%',
+    paddingLeft: '1%',
+    alignItems: 'center',
   },
   mainHead: {
     fontSize: 25,
     fontWeight: '900',
     marginVertical: '3%',
     marginHorizontal: '2%',
-    color: '#484848'
-  }
+    color: '#484848',
+  },
 });
